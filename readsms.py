fbff7267c2e7606e8db2c04f32b38ed5e878c799
deaq42 <62000951+deaq42@users.noreply.github.com>
2020-04-02 10:11:23 +0500

    командная строка

--- readsms.py
+++ readsms.py
@@ -5,28 +5,40 @@ import time
 import hashlib
 import requests
 import urllib
-SERVER_ADRESS = 'http://localhost/?query='
-PHONE_NUM = {}   #Словарь в котором будут хранится данные об известных номерах и соответсвующий ТС. {'phone_number':'К:КП:Объект'}
-commandkey='1234'
+import sys
+import argparse
 
-def oik_set_ts(ts_adress):       
-    timestamp = str(time.time())
-    timestamp = timestamp.split('.')
-    timestamp = timestamp[0] + '000' #Готовим метку времени формата ОИК диспетчер
-    str2 = f'!IP-gate@{commandkey}@{timestamp}!' #Готовим и солим хеш запроса
-    sha_str = hashlib.sha256(str2.encode())
-    #print (sha_str.hexdigest())
-    #print (timestamp)
-    #query_test='{%22id%22:%22test-client%22,%22query%22:%22test%22}'
-    #print (urllib.parse.unquote(query_test))
-    query2 = '{%22query%22:%20%22set-ts%22,%22ts%22:%20%22'+ts_adress+'%22,%20%20%22value%22:%201,%22hash%22:%20"'+sha_str.hexdigest()+'",%22timestamp%22:%20'+timestamp+'%20}' 
-    print (urllib.parse.unquote(query2))
-    #print (query.items())            
-    full_url = SERVER_ADRESS + query2
-    #responce = requests.get(SERVER_ADRESS, params = query)
-    responce = requests.get(full_url)
-    print (responce.text)
-    print (responce.url)
+SERVER_ADRESS = 'http://localhost/' #+++Сделать считывание этого парамтера из командной строки++++
+PHONE_NUM = {}   #Словарь в котором будут хранится данные об известных номерах и соответсвующий ТС. {'phone_number':'К:КП:Объект'}
+COM_PORT = 'COM5'
+COM_SPEED = '115200'
+commandkey='1234' #ключ для соления хеша, задается в параметрах запуска оик_хттп_гейт
+
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('-s', '--server', default='127.0.0.1')
+    parser.add_argument('-p', '--port', default = 'none')
+    parser.add_argument('-cc', '--comandkey', default = 'none') 
+    parser.add_argument('-cs', '--comspeed', default = '115200')
+
+    return parser
+parser = createParser()
+namespace = parser.parse_args(sys.argv[1:])
+    # print (namespace)
+if namespace.port == 'none':
+    print ('Не задан ком порт')
+    raise SystemExit
+if namespace.comandkey == 'none':
+    print ('не задан ключ управления ОИК!')
+    raise SystemExit
+SERVER_ADRESS = 'http://'+namespace.server+'/'
+COM_PORT = namespace.port
+commandkey = namespace.comandkey
+COM_SPEED = namespace.comspeed
+print ("Адрес сервера {}".format (SERVER_ADRESS) )
+print ("КОМ ПОРТ {}".format (namespace.port) )
+print ("КОМ ПОРТ СКОРОСТЬ {}".format (namespace.comspeed) )
+print ("КЛЮЧ {}".format (namespace.comandkey) )
 
 
 def load_settings():
@@ -34,20 +46,58 @@ def load_settings():
     tp_numbers = f.read().splitlines()
     #first = tp_numbers[0].split(';')
     f.close()
-    print (len(tp_numbers))
+    print (f'Количество записей в бд : {len(tp_numbers)}')
     for number in tp_numbers:
-        #number.split(';')
-        PHONE_NUM[number[0:12]] = number[13:21]
+        number = number.split(';')
+        PHONE_NUM[number[0]] = number[1] #Сделать нормальный разбор по разделителям а не по количеству символов
         print (number[0:12])
-    print (PHONE_NUM)
+    #print (PHONE_NUM)
     #return tp_numbers
-load_settings()
 
-ser = serial.Serial('COM5', 115200, dsrdtr = 1,timeout = 0) #открываем порт
+    #Инициализация порта
+load_settings()
+try:
+    ser = serial.Serial(COM_PORT, 115200, dsrdtr = 1,timeout = 0) #открываем порт
+except:
+    print (f'Невозоможно открыть {COM_PORT}')
+    raise SystemExit
 ser.write(bytes ('AT+CMGF=1 \r\n','utf8')) # #переводим режим ответетов в текст
 time.sleep(0.2)
-ser.write(bytes ('\f','utf8')) #Очищаем экран, не уверен что нужно
+#ser.write(bytes ('\f','utf8')) #Очищаем экран, не уверен что нужно
 time.sleep(2)
+    #конец инициализации
+
+def oik_set_ts(ts_adress):       
+    timestamp = str(time.time())
+    timestamp = timestamp.split('.')
+    timestamp = timestamp[0] + '000' #Готовим метку времени формата ОИК диспетчер
+    str2 = f'!IP-gate@{commandkey}@{timestamp}!' #Готовим и солим хеш запроса
+    sha_str = hashlib.sha256(str2.encode())
+    query2 = '?query={%22query%22:%20%22set-ts%22,%22ts%22:%20%22'+ts_adress+'%22,%20%20%22value%22:%201,%22hash%22:%20"'+sha_str.hexdigest()+'",%22timestamp%22:%20'+timestamp+'%20}' 
+    print (urllib.parse.unquote(query2))
+    #print (query.items())            
+    full_url = SERVER_ADRESS + query2
+    print (full_url)
+    #responce = requests.get(SERVER_ADRESS, params = query)
+    responce = requests.get(full_url)
+    print (responce.text)
+    print (responce.url)
+
+
+def oik_switch_ts(ts_adress):
+    timestamp = str(time.time())
+    timestamp = timestamp.split('.')
+    timestamp = timestamp[0] + '000' #Готовим метку времени формата ОИК диспетчер
+    str2 = f'!IP-gate@{commandkey}@{timestamp}!' #Готовим и солим хеш запроса
+    sha_str = hashlib.sha256(str2.encode())
+    query2 = '?query={%22query%22:%20%22switch-ts%22,%22ts%22:%20%22'+ts_adress+'%22,%22hash%22:%20"'+sha_str.hexdigest()+'",%22timestamp%22:%20'+timestamp+'%20}'    
+    full_url = SERVER_ADRESS + query2
+    #responce = requests.get(SERVER_ADRESS, params = query)
+    responce = requests.get(full_url)
+    print (responce.text)
+    print (responce.url)    
+
+
 
 def check_sms():
     ser.reset_output_buffer
@@ -60,6 +110,16 @@ def check_sms():
         print (read_l)    
         readsms(read_l[-1]) #Вызываем функцию реадсмс с аргументом в котором передается номер ячейки памяти с новым сообщением
     return read_l[-1]
+def check_at():
+    ser.reset_output_buffer
+    ser.write(bytes (f'AT\r\n', 'utf8'))
+    read_l = ser.read(600).decode('utf8').split(',')#Читаем данные из ком порта и разделяем их по запятой
+    print ('AT')
+    print (read_l[0])
+    #print (len(read_l[-1]))
+    print ('------')
+    #print (read_l[1])
+
 def readsms(cell_n):
     command_read = bytes (f'AT+CMGR={cell_n} \r\n', 'utf8')   
     ser.write(command_read)
@@ -74,10 +134,13 @@ def readsms(cell_n):
     time.sleep(0.5)
     ser.write(bytes ('AT+CMGDA="DEL INBOX" \r\n', 'utf8')) #Удалить смс все
     #print ('full_resp ')
+    print ('---\n Входящее сообщение!\n')
     print (f'Номер телефона: {sms_phone_number}, этот номер должен изменить ТС {PHONE_NUM[sms_phone_number]}')
-    print (f'Время {sms_time}')
-    print (f'Сообщение {sms_text}')
-    oik_set_ts(PHONE_NUM[sms_phone_number])
+    print (f'Время: {sms_time}')
+    print (f'Текст сообщения: {sms_text[0]}')
+    print ('\n')
+    #Добавить условие наличия sms_phone_number в словаре PHONE_NUM
+    oik_switch_ts(PHONE_NUM[sms_phone_number])
     str_2_log = f'{sms_phone_number};{PHONE_NUM[sms_phone_number]}{sms_time};{sms_text[0]} \n'#Довести до ума строку. удалить лишние символы в конце
     f_log = open('py_gsm_log.log', 'a')
     f_log.writelines(str_2_log)
@@ -88,8 +151,13 @@ def readsms(cell_n):
     
 
 #readsms(1)
+a=0
 while True:
+    
+    a=a+1
     check_sms()
-
+    if a == 10:
+        check_at()
+        a = 0
     time.sleep(2)
 ser.close()
